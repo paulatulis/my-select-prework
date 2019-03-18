42e6709801c2522fdedb1758e572926a953e2466
Paula Tulis <tulis.paula@gmail.com>
2019-03-18 00:30:10 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i=0 
+ new_collection = []
+ while i < collection.length 
+if yield(collection[i]) == true 
+  new_collection << collection[i]
 end
+i = i+1 
+end 
+new_collection 
+end 
+
